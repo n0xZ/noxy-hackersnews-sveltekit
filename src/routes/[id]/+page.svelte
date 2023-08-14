dddedb1e1e4d839df8be4afc2221c58407915829
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-08-14 20:52:37 -0300

    init dynamic page

---
+++ src/routes/[id]/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+</script>
+
+<main class="flex flex-col container mx-auto max-w-4xl prose prose-sm mt-7">
+	<h2 class="text-center">{data.relatedPost.title}</h2>
+	<small class="mb-10 opacity-70">{data.relatedPost.by}</small>
+	{#if data.relatedPost.text}
+		<div contenteditable="true" bind:innerHTML={data.relatedPost.text} />
+	{:else}
+		<a href={data.relatedPost.url}>Related post</a>
+	{/if}
+</main>
