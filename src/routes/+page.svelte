31603bc654f5656930d5cb6785e3d1789b50f7bf
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-08-14 20:59:20 -0300

    chore: add titles on each page

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+
+<svelte:head>
+<title>Tukinews</title>
+</svelte:head>
+<main class="h-full grid place-items-center prose prose-sm container mx-auto mt-10 w-full">
+	<h2>Tuki news al rescate!</h2>
+	<p>Elija el post a leer.</p>
+</main>
--- src/routes/[id]/+page.svelte
+++ src/routes/[id]/+page.svelte
@@ -4,6 +4,9 @@
 	export let data: PageData;
 </script>
 
+<svelte:head>
+	<title>Tukinews : {data.relatedPost.title}</title>
+</svelte:head>
 <main class="flex flex-col container mx-auto max-w-4xl prose prose-sm mt-7">
 	<h2 class="text-center">{data.relatedPost.title}</h2>
 	<small class="mb-10 opacity-70">{data.relatedPost.by}</small>
